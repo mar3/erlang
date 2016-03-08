0e7c49d0148477120197cc76fdf12d3b17ea6e0b
mass10 <irisawa.masaru@gmail.com>
2016-03-08 13:40:33 +0000

    wip

--- code/filesystem/cat/cat.erl
+++ code/filesystem/cat/cat.erl
@@ -4,13 +4,9 @@
 
 print_line(Handle) ->
 
-	%
 	% ファイルの読出し操作。再帰呼び出しで繰り返しを実現している。
-	%
 
-	%
 	% get_line の戻り値による処理分岐。Erlang ではパターンマッチングと呼ぶ。
-	%
 	case io:get_line(Handle, "") of
 		eof ->
 			ok;
@@ -23,26 +19,18 @@ main(Path) ->
 
 	try
 
-		%
 		% ファイルを開く
-		%
 		{ok, Handle} = file:open(Path, read),
 
-		%
 		% 読み出し。ファイルの終端まで繰り返す。
-		%
 		print_line(Handle),
 
-		%
 		% ファイルを閉じる
-		%
 		file:close(Handle)
 
 	catch
 
-		%
 		% 例外処理
-		%
 		error:Reason ->
 			io:format( "[ERROR] caught an exception: ~w~n", [Reason] )
 

abd6cb1dfa82ef336d6ef301c6a25ccc201d2990
mass10 <mass10.github@gmail.com>
2016-10-02 14:27:55 +0000

    wip

--- code/higher-order-function/main.erl
+++ code/higher-order-function/main.erl
@@ -3,12 +3,34 @@
 
 
 
-invoke(Function, Vaule) ->
-	Function(Vaule).
+invoke(Operation, Vaule) ->
+
+	%%
+	%% 第一引数 Operation が関数であるとみなして Operation() を呼び出してみます。
+	%% 引数は Value です。
+	%%
+
+	Operation(Vaule).
 
 main(_) ->
-	Operation = fun(X) -> X * 2 end,
-	Result = invoke(Operation, 999),
-	io:format("~p~n", [Result]),
-	done.
 
+	%%
+	%% Erlang の無名関数を Operation という変数に束縛しています。
+	%%
+
+	Operation = fun(X) -> X * 2 end,
+
+	%%
+	%% Operation を渡して invoke() を呼び出しています。第二引数は 7 です。
+	%% invoke() の結果が Result に束縛されることになります。
+	%%
+
+	Result = invoke(Operation, 7),
+
+	%%
+	%% Result を表示しています。
+	%%
+
+	io:format("~p~n", [Result]),
+
+	done.
